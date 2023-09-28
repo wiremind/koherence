1cf147bb0de16512d5faf6947f133913dc8c9921
Anthony Delannoy <adelannoy@wiremind.io>
2023-09-28 12:15:02 +0200

    chore: add version files

---
+++ version/version.go
@@ -0,0 +1,17 @@
+package version
+
+const (
+	defaultVersion = "v0.0.0+unknown"
+)
+
+var (
+	// Package is filled at linking time
+	Package = "github.com/wiremind/koherence"
+
+	// Version holds the complete version number. Filled in at linking time.
+	Version = defaultVersion
+
+	// Revision is filled with the VCS (e.g. git) revision being used to build
+	// the program at linking time.
+	Revision = ""
+)
